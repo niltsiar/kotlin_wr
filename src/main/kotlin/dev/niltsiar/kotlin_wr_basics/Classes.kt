2adafbf0a5946a6f28a876b168dc08809acbcd78
Borja Quevedo <borjota@gmail.com>
2020-11-14 23:28:51 +0100

    ✨ Add classes

---
+++ src/main/kotlin/dev/niltsiar/kotlin_wr_basics/Classes.kt
@@ -0,0 +1,22 @@
+@file:Suppress("unused")
+
+package dev.niltsiar.kotlin_wr_basics
+
+class Business(
+    val name: String,
+    members: List<Member> = emptyList()
+) {
+
+    private val _members: MutableList<Member> = members.toMutableList()
+    val members: List<Member>
+        get() = _members
+}
+
+
+abstract class Member(
+    open val name: String
+)
+
+data class Subscriber(override val name: String) : Member(name)
+data class DungeonMaster(override val name: String) : Member(name)
+data class Minion(override val name: String, val position: String) : Member(name)
