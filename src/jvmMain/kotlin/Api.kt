97372eafa0ebc790bf61f75fc6a74995a7fc3ea5
Borja Quevedo <borjota@gmail.com>
2020-11-18 19:56:37 +0100

    ✨ Add pipelines

--- src/jvmMain/kotlin/Api.kt
+++ src/jvmMain/kotlin/Api.kt
@@ -1,11 +1,28 @@
 package dev.niltsiar.kotlin_wr
 
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun Application.api() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    install(CORS) {
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Delete)
+        anyHost()
+    }
+
+    install(Compression) {
+        gzip()
+    }
+
     routing {
         get("/") {
             call.respondText("Hola, malandriners", ContentType.Text.Plain)
