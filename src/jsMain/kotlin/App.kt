20b5c604eaf8cf1671a7dd8d14f1b7a94c96d7e8
Borja Quevedo <borjota@gmail.com>
2020-11-19 01:08:00 +0100

    ✨ Add header

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,5 +1,21 @@
 package dev.niltsiar.kotlin_wr
 
+import dev.fritz2.binding.const
+import dev.fritz2.dom.append
+import dev.fritz2.dom.html.render
+
 fun main() {
 
+    val inputHeader = render {
+        header {
+            h1 { +"¿Trello? 🙄" }
+
+            input("new-todo") {
+                placeholder = const("Oye, ¿qué tienes que hacer? 🤔")
+                autofocus = const(true)
+            }
+        }
+    }
+
+    append("todoapp", inputHeader)
 }
